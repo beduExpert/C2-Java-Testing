019729a632a475ad7423c4a9362aedf26fafa51b
Alex Montoya <amonmar2000@hotmail.com>
2019-10-25 00:05:13 -0500

    Se agregan los assets necesarios para el desarrollo del Ejemplo-01

---
+++ Sesion-01/Ejemplo-01/src/test/java/org/bedu/testing/introduction/SumadorLunarTest.java
@@ -0,0 +1,14 @@
+package org.bedu.testing.introduction;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SumadorLunarTest {
+
+	@Test
+	void sumaLunar() {
+		fail("Not yet implemented");
+	}
+
+}
