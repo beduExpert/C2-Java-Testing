34f4db3e9fbc554f75f587dbc2532ec10f8d4296
Alex Montoya <amonmar2000@hotmail.com>
2019-10-30 21:05:52 -0600

    Se agrega el código del ejemplo-02

---
+++ Sesion-03/Ejemplo-02/kata-fizzbuzz/src/main/java/org/bedu/testing/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,23 @@
+package org.bedu.testing.fizzbuzz;
+
+public class FizzBuzz {
+	public static String valorSecuencia(int valor) {
+
+		String regreso = "";
+		
+		if(valor == 0) {
+			return "0";
+		}
+		
+		if(valor % 3 == 0) {
+			regreso +=  "Fizz";
+		} 
+		
+		if(valor % 5 == 0) {
+			regreso += "Buzz";
+		}
+		
+		
+		return regreso.isEmpty() ? String.valueOf(valor) : regreso;
+	}
+}
---
+++ Sesion-03/Ejemplo-02/kata-fizzbuzz/src/test/java/org/bedu/testing/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,44 @@
+package org.bedu.testing.fizzbuzz;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+
+	@Test
+	void ceroEsCero() {
+		assertEquals("0", FizzBuzz.valorSecuencia(0));
+	}
+	
+	@Test
+	void unoEsUno() {
+		assertEquals("1", FizzBuzz.valorSecuencia(1));
+	}
+	
+	@Test
+	void tresEsFizz() {
+		assertEquals("Fizz", FizzBuzz.valorSecuencia(3));
+	}
+	
+	@Test
+	void cincoEsBuzz() {
+		assertEquals("Buzz", FizzBuzz.valorSecuencia(5));
+	}
+	
+	@Test 
+	void seisEsFizz() {
+		assertEquals("Fizz", FizzBuzz.valorSecuencia(6));
+	}
+
+	@Test 
+	void diezEsBuzz() {
+		assertEquals("Buzz", FizzBuzz.valorSecuencia(10));
+	}
+	
+	@Test 
+	void quinceEsFizzBuzz() {
+		assertEquals("FizzBuzz", FizzBuzz.valorSecuencia(15));
+	}
+	
+}
