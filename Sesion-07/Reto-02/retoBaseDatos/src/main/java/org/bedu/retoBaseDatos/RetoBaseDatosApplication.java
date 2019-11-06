46d585dc40dab1e34a87bb214990a3aa409672ca
Alex Montoya <amonmar2000@hotmail.com>
2019-11-05 23:45:54 -0600

    Se agrega el código del reto 02

---
+++ Sesion-07/Reto-02/retoBaseDatos/src/main/java/org/bedu/retoBaseDatos/RetoBaseDatosApplication.java
@@ -0,0 +1,13 @@
+package org.bedu.retoBaseDatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoBaseDatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoBaseDatosApplication.class, args);
+	}
+
+}
---
+++ Sesion-07/Reto-02/retoBaseDatos/src/test/java/org/bedu/retoBaseDatos/RetoBaseDatosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.retoBaseDatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoBaseDatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
