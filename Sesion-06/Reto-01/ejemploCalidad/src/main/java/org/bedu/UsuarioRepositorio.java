b6ba18f208638880b9e88dafe701995edefbbeea
Alex Montoya <amonmar2000@hotmail.com>
2019-11-07 22:03:58 -0600

    Se agrega el código del Reto 01

---
+++ Sesion-06/Reto-01/ejemploCalidad/src/main/java/org/bedu/UsuarioRepositorio.java
@@ -0,0 +1,51 @@
+package org.bedu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuarioRepositorio {
+    List usuarios;
+
+    public UsuarioRepositorio() {
+        this.usuarios = new ArrayList();
+    }
+
+    public Usuario guardar(Usuario usuario) {
+        usuario.setId(usuarios.size());
+        if(usuarios.add(usuario) == true)
+            return usuario;
+        else
+            return null;
+    }
+
+    public Usuario buscarPorId(int id){
+        Usuario encontrado = null;
+        try{
+            encontrado = (Usuario) usuarios.get(id);
+        }catch (Exception e){
+
+        }
+        return encontrado;
+    }
+
+    public Usuario reemplazar(int id, Usuario nuevo){
+        Usuario guardado = null;
+        nuevo.setId(id);
+        try{
+            guardado = (Usuario) usuarios.set(id, nuevo);
+        } catch(Exception e){
+
+        }
+        return guardado;
+    }
+
+    public boolean eliminar(int id){
+        boolean resultado = true;
+        try{
+            usuarios.remove(id);
+        }catch (Exception e){
+            resultado = false;
+        }
+        return resultado;
+    }
+}
