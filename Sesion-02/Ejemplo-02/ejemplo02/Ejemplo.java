100ddb749a7ea897460e82d03d4532cae411f7e9
Alex Montoya <amonmar2000@hotmail.com>
2019-11-11 23:36:02 -0600

    Se agrega el código del Ejemplo 02

---
+++ Sesion-02/Ejemplo-02/ejemplo02/Ejemplo.java
@@ -0,0 +1,19 @@
+public class Ejemplo{
+
+  public static void main (String... args){
+    while (a < 3){
+		if (b < 1){
+			c++;
+		}
+		if (a > b) {
+			b = a;
+		}
+		else if (c == 3){
+			c++;
+		}
+		else {
+			a++;
+		}
+	}
+  }
+}
